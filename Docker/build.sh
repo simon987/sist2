317034ba2147a53effb0ff642e2f2483586231bf
simon987 <me@simon987.net>
2020-03-24 10:01:27 -0400

    teamcity automation attempt

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,15 +1,13 @@
 rm ./sist2
-cp ../sist2 .
+cp ../sist2 ../sist2_debug .
 strip sist2
 
 version=$(./sist2 --version)
 
 echo "Version ${version}"
-docker build . -t simon987/sist2:${version} -t simon987/sist2:latest \
-  -t docker.pkg.github.com/simon987/sist2/sist2:latest -t docker.pkg.github.com/simon987/sist2/sist2:${version}
+docker build . -t simon987/sist2:${version} -t simon987/sist2:latest
+
 docker push simon987/sist2:${version}
 docker push simon987/sist2:latest
-docker push docker.pkg.github.com/simon987/sist2/sist2:latest
-docker push docker.pkg.github.com/simon987/sist2/sist2:${version}
 
 docker run --rm -it simon987/sist2 -v
