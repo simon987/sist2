718169345e741cf3b9b1af24d86e6d53fc27f0e5
simon987 <me@simon987.net>
2020-04-21 19:10:57 -0400

    gzip artifacts in CI

--- ci/build.sh
+++ ci/build.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 
+rm *.gz
+
 rm -rf CMakeFiles CMakeCache.txt
 cmake -DSIST_DEBUG=off -DCMAKE_TOOLCHAIN_FILE=/vcpkg/scripts/buildsystems/vcpkg.cmake .
 make
 strip sist2
+gzip -9 sist2
 
 rm -rf CMakeFiles CMakeCache.txt
 cmake -DSIST_DEBUG=on -DCMAKE_TOOLCHAIN_FILE=/vcpkg/scripts/buildsystems/vcpkg.cmake .
 make
+cp /usr/lib/x86_64-linux-gnu/libasan.so.2.0.0 libasan.so.2
+tar -czf sist2_debug.tar.gz sist2_debug libasan.so.2
