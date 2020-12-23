bc120f349d91b1592ea5a08c104e45dcf47f114e
simon987 <me@simon987.net>
2020-12-23 10:26:26 -0500

    Setup ARM CI builds

--- ci/build_arm64.sh
+++ ci/build_arm64.sh
@@ -4,6 +4,8 @@ VCPKG_ROOT="/vcpkg"
 
 rm *.gz
 
+git submodule update --init --recursive
+
 rm -rf CMakeFiles CMakeCache.txt
 cmake -DSIST_DEBUG=off -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" .
 make -j 4
