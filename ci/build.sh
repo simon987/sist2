f5bbe0dc97c9699a60c77841ef49ab4b059ca25d
simon987 <me@simon987.net>
2020-12-31 10:54:21 -0500

    Update binary names

--- ci/build.sh
+++ ci/build.sh
@@ -10,10 +10,12 @@ rm -rf CMakeFiles CMakeCache.txt
 cmake -DSIST_DEBUG=off -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" .
 make -j 33
 strip sist2
-gzip -9 sist2
+mv sist2 sist2-x64-linux
+gzip -9 sist2-x64-linux
 
 rm -rf CMakeFiles CMakeCache.txt
 cmake -DSIST_DEBUG=on -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" .
 make -j 33
 cp /usr/lib/x86_64-linux-gnu/libasan.so.2.0.0 libasan.so.2
-tar -czf sist2_debug.tar.gz sist2_debug libasan.so.2
+mv sist2_debug sist2-x64-linux-debug
+tar -czf sist2_debug.tar.gz sist2-x64-linux-debug libasan.so.2
--- ci/build_arm64.sh
+++ ci/build_arm64.sh
@@ -10,5 +10,5 @@ rm -rf CMakeFiles CMakeCache.txt
 cmake -DSIST_DEBUG=off -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" .
 make -j 4
 strip sist2
-mv sist2 sist2_arm64
-gzip -9 sist2_arm64
+mv sist2 sist2-arm64-linux
+gzip -9 sist2-arm64-linux
