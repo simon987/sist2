6a48b219e60fcf47f458f6bf9dcfa9bfe1bb35ce
simon987 <me@simon987.net>
2021-09-05 12:19:44 -0400

    Fix build_arm64.sh & update README

--- ci/build_arm64.sh
+++ ci/build_arm64.sh
@@ -8,7 +8,7 @@ rm -rf CMakeFiles CMakeCache.txt
 cmake -DSIST_PLATFORM=arm64_linux -DSIST_DEBUG=off -DBUILD_TESTS=off -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" .
 make -j $(nproc)
 strip sist2
-mv sist2 sist2-arm64-linux
+mv sist2_debug sist2-arm64-linux-debug
 
 rm -rf CMakeFiles CMakeCache.txt
 cmake -DSIST_PLATFORM=arm64_linux -DSIST_DEBUG=on -DBUILD_TESTS=off -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" .
