52466d5d8af8758ea5b9fa284526f456e2ca267c
simon987 <me@simon987.net>
2021-12-25 11:12:00 -0500

    Update tesseract datapaths

--- src/cli.c
+++ src/cli.c
@@ -22,6 +22,7 @@
 const char *TESS_DATAPATHS[] = {
         "/usr/share/tessdata/",
         "/usr/share/tesseract-ocr/tessdata/",
+        "/usr/share/tesseract-ocr/4.00/tessdata/",
         "./",
         NULL
 };
